eccf6974ee30d5c8dc388bf1c59972a839396b42
Malihatmalam <malihatmalam@gmail.com>
2021-01-02 22:10:53 +0700

    Seeder Address
    Seeder Provinsi, kota dan kecamatan (district)

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        // Menjalankan seeder  provinsi, kota dan kecamatan
+        $this->call(ProvinceTableSeeder::class);
+        $this->call(CityTableSeeder::class);
+        $this->call(DistrictTableSeeder::class);
     }
 }
